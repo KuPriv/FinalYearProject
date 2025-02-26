cb961a8c603f0deeacd7179ba34b7523dfdbd3fc
Egor Sezen <sezen.egor@mail.ru>
2025-02-26 19:10:27 +0300

    read from file (first version)

---
+++ read_from_file.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+name_folder = 'generated_files'
+
+def main() -> None:
+    matrix = get_data_from_file()
+    print(matrix)
+
+def get_data_from_file() -> list[list[str]]:
+    count_of_files: int = count_files()
+    matrix = []
+    for i in range(count_of_files):
+        with open(name_folder + fr'/{i + 1}.txt') as file:
+            data = file.readlines()
+            data = [line.rstrip() for line in data]
+            row = [number for number in data]
+            matrix.append(row)
+    return matrix
+
+def count_files() -> int:
+    # returns: количество файлов в папке generated_files
+    folder = Path(name_folder)
+    return len(list(folder.iterdir()))
+
+
+if __name__ == "__main__":
+    main()
