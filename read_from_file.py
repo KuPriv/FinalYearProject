3a6e916911edb33f96cbfabf7c0ec86d967882d0
Egor Sezen <sezen.egor@mail.ru>
2025-03-17 10:50:37 +0300

    change type of matrix

--- read_from_file.py
+++ read_from_file.py
@@ -3,18 +3,21 @@ name_folder = 'generated_files'
 
 def main() -> None:
     matrix = get_data_from_file()
-    print(matrix)
+    for matrix in matrix:
+        print(matrix)
 
-def get_data_from_file() -> list[list[str]]:
+
+def get_data_from_file() -> list:
     count_of_files: int = count_files()
     matrix = []
     for i in range(count_of_files):
         with open(name_folder + fr'/{i + 1}.txt') as file:
             data = file.readlines()
-            data = [line.rstrip() for line in data]
+            data = [line.rstrip().split() for line in data]
             row = [number for number in data]
             matrix.append(row)
     return matrix
+
 
 def count_files() -> int:
     # returns: количество файлов в папке generated_files
