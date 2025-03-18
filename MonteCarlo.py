6833ebe3eb95b288e6bee4401c8990625da7b635
Egor Sezen <sezen.egor@mail.ru>
2025-03-18 19:18:20 +0300

    MonteCarlo method!

---
+++ MonteCarlo.py
@@ -0,0 +1,39 @@
+from random import shuffle
+from xmlrpc.client import MAXINT
+
+import read_from_file
+
+
+def calc_distance(way, matrix) -> int:
+    return sum(matrix[way[i]][way[i+1]] for i in range(len(way)-1)) + matrix[way[-1]][way[0]]
+
+
+def monte_carlo(matrix, iters: int) -> tuple:
+    n = len(matrix)
+    cities = list(range(n))
+    min_dist: int = MAXINT
+    best_way = None
+
+    for _ in range(iters):
+        current_way = cities[:]
+        # shuffle - из библиотеки random, работает быстрее, так как локальная область видимости
+        shuffle(current_way)
+        cur_dist = calc_distance(current_way, matrix)
+        if cur_dist < min_dist:
+            min_dist = cur_dist
+            print(best_way)
+            print(current_way)
+            best_way = current_way[:]
+
+    return best_way, min_dist
+
+def main() -> None:
+    matrix = read_from_file.main()
+    for matrix in matrix:
+        way, distance = monte_carlo(matrix, iters=10000)
+        print("Лучший маршрут:", way)
+        print("Кратчайшее расстояние:", distance)
+
+
+if __name__ == "__main__":
+    main()
