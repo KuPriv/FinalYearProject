131d51dc60c00a633143ddb1573465367db84673
Egor Sezen <sezen.egor@mail.ru>
2025-04-05 18:20:37 +0300

    correct positions funcs in code

--- BranchAndBound.py
+++ BranchAndBound.py
@@ -1,4 +1,6 @@
+import math
 import time
+from random import choices
 from xmlrpc.client import MAXINT
 
 import read_from_file
@@ -14,7 +16,7 @@ def time_counter(func):
     return wrapper
 
 
-@time_counter
+#@time_counter
 def branch_and_bound(matrix: list) -> tuple:
     n = len(matrix)
     min_dist: int = MAXINT
@@ -38,12 +40,38 @@ def branch_and_bound(matrix: list) -> tuple:
     return best_way, min_dist
 
 
+def markov_chain(matrix, n, alpha: float = 1.0) -> tuple:
+    cur_state = 0
+    way = [0]
+    unvisited = set(range(n))
+    unvisited.remove(cur_state)
+
+    while unvisited:
+        available_states = list(unvisited)
+        weights = [math.exp(-alpha * matrix[cur_state][j]) for j in available_states]
+        sum_weight = sum(weights)
+        calc_weights = [w / sum_weight for w in weights]
+
+        next_state = choices(available_states, weights=calc_weights, k=1)[0]
+        way.append(next_state)
+        unvisited.remove(next_state)
+        cur_state = next_state
+
+    dist = calc_distance(way, matrix)
+    return way, dist
+
+
+def calc_distance(way, matrix) -> int:
+    return sum(matrix[way[i]][way[i+1]] for i in range(len(way)-1)) + + matrix[way[-1]][way[0]]
+
+
 def main() -> None:
     matrices = read_from_file.main()
     for matrix in matrices:
         way, distance = branch_and_bound(matrix)
-        print("Лучший маршрут:", way)
-        print("Кратчайшее расстояние:", distance)
+        # print("Лучший маршрут:", way)
+        # print("Кратчайшее расстояние:", distance)
+        print(distance)
 
 
 if __name__ == "__main__":
