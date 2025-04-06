e152cc037dbd5c6d7676e1ce8c0ad641e86f5105
Egor Sezen <sezen.egor@mail.ru>
2025-04-06 15:26:58 +0300

    changed logic of reading files, did it with UI

--- read_from_file.py
+++ read_from_file.py
@@ -1,16 +1,13 @@
 from pathlib import Path
+
 name_folder = 'generated_files'
 
-def main() -> list:
-    matrix = get_data_from_file()
-    return matrix
 
-
-def get_data_from_file() -> list:
-    count_of_files: int = count_files()
+def get_data_from_all_files(n: int) -> list:
+    count_of_files: int = count_files(n)
     matrix = []
     for i in range(count_of_files):
-        with open(name_folder + fr'/{i + 1}.txt') as file:
+        with open(name_folder + fr'/{n}/{i + 1}.txt') as file:
             data = file.readlines()
             data = [list(map(int, line.rstrip().split())) for line in data]
             row = [number for number in data]
@@ -18,10 +15,24 @@ def get_data_from_file() -> list:
     return matrix
 
 
-def count_files() -> int:
-    # returns: количество файлов в папке generated_files
-    folder = Path(name_folder)
+def get_data_from_one_file(n: int, file_n: int) -> list:
+    matrix = []
+    with open(name_folder + fr'/{n}/{file_n}.txt') as file:
+        data = file.readlines()
+        data = [list(map(int, line.rstrip().split())) for line in data]
+        row = [number for number in data]
+        matrix.append(row)
+    return matrix
+
+
+def count_files(n: int) -> int:
+    # returns: количество файлов в папке generated_files/n, n - номер папки
+    folder = Path(name_folder + f'/{n}')
     return len(list(folder.iterdir()))
+
+
+def main() -> None:
+    ...
 
 
 if __name__ == "__main__":
